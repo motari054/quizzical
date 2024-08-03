0b0195effac6e0aa16f1f654752fd6b49747f545
motari054 <samuelmotari54@gmail.com>
2024-08-03 17:23:02 +0300

    home page created

---
+++ quiz/src/components/Home.jsx
@@ -0,0 +1,11 @@
+export function Home(){
+    return (
+        <>
+        <div className="home--page">
+            <h1>Quizzical</h1>
+            <h4>Some Description needed</h4>
+            <button>Start Quiz</button>
+        </div>
+        </>
+    )
+}
---
+++ quiz/src/components/QuestionsPage.jsx
@@ -0,0 +1,89 @@
+import { Quiz } from "./Quiz";
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti'
+
+export function QustionsPage() {
+    const [trivia, setTrivia] = useState([])
+    const [score, setScore] = useState(0)
+    const [isSubmitted, setIsSubmited] = useState(false)
+
+    useEffect(()=>{
+    fetchATriviaQuestions()
+    }, [])
+
+    const fetchATriviaQuestions = (()=> {
+    fetch('https://the-trivia-api.com/v2/questions')
+        .then(res => res.json())
+        .then(result => {
+        const updatedTrivia = result.map(question => {
+            const correct = { answer: question.correctAnswer, isHeld: false, id: nanoid() };
+            const incorrect = question.incorrectAnswers.map(answer => ({
+            answer, id: nanoid(), isHeld: false
+            }));
+            let allAnswers = [...incorrect, correct]
+            const shuffleAllAnswers = allAnswers.sort((a,b)=> 0.5 - Math.random())
+            allAnswers = shuffleAllAnswers
+            return { ...question, allAnswers, questionText: question.question }
+        });
+        setTrivia(updatedTrivia);
+        });
+    })
+
+    function selectAnswer(questionId, answerId) {
+    setTrivia(prevTrivia =>
+    prevTrivia.map(question => {
+        if (question.id !== questionId) return question
+        const updatedAnswers = question.allAnswers.map(answer => ({
+        ...answer,
+        isHeld: answer.id === answerId
+        }));
+        return { ...question, allAnswers: updatedAnswers }
+      })
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const score = trivia.reduce((acc, question) => {
+      const selectedAnswer = question.allAnswers.find(answer => answer.isHeld);
+      return acc + (selectedAnswer && selectedAnswer.answer === question.correctAnswer ? 1 : 0)
+    }, 0);
+    setScore(score)
+    setIsSubmited(true)
+  }
+  
+  const everyQuestionAnswered = trivia.every(question =>
+    question.allAnswers.some(answer => answer.isHeld)
+  )
+
+  function handleTryAgain(){
+    fetchATriviaQuestions()
+  }
+
+  return (
+    <>
+    <div className="main">
+    <form action="">
+      {trivia.map(trivia => (
+        <Quiz
+          key={trivia.id}
+          question={trivia.question}
+          choices={trivia.allAnswers}
+          selectAnswer={(answerId) => selectAnswer(trivia.id, answerId)}
+        />
+      ))}
+      <div className="submit--button">
+        {isSubmitted && score >= 8 ? <Confetti/> : ''}
+        {isSubmitted && (
+          <div className="score--text">You scored {score} out of {trivia.length}</div>
+        )}
+        <button onClick={isSubmitted ? handleTryAgain : handleSubmit} disabled={!everyQuestionAnswered}>
+          {isSubmitted ? 'Try Again' : 'Submit'}
+        </button>
+      </div>
+    </form>
+    </div>
+    </>
+  );
+}
